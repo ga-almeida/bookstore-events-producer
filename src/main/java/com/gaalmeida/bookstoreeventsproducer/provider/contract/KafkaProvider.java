f37d8dcfd7b6f22dae283a1e58050bbf56d86b13
ga-almeida <gabrielsantos.almeida@hotmail.com>
2021-11-29 22:56:13 -0300

    refactor

--- src/main/java/com/gaalmeida/bookstoreeventsproducer/provider/contract/KafkaProvider.java
+++ src/main/java/com/gaalmeida/bookstoreeventsproducer/provider/contract/KafkaProvider.java
@@ -9,8 +9,8 @@ import org.springframework.util.concurrent.ListenableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
-public interface KafkaProvider {
-    void sendLibraryEvent(BookStore bookStore, BookStoreEventType bookStoreEventType) throws JsonProcessingException;
+public interface KafkaProvider<T> {
+    void sendLibraryEvent(T bookStore, BookStoreEventType bookStoreEventType) throws JsonProcessingException;
     ListenableFuture<SendResult<Integer, String>> senBookStoreEvent(BookStore bookStore, BookStoreEventType bookStoreEventType) throws JsonProcessingException;
     SendResult<Integer, String> sendBookStoreEventSynchronous(BookStore bookStore, BookStoreEventType bookStoreEventType) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException;
 }
